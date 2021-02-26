3b9d8aaee30c6f879bfe0238de93d6543ceb67ea
AboaliFcih <mohamed.ali.fcih1992@gmail.com>
2021-02-26 20:13:54 +0400

    ADD API TO ENV

--- src/redux/search/action.ts
+++ src/redux/search/action.ts
@@ -8,7 +8,7 @@ export type AppThunk = ActionCreator<ThunkAction<void, null, null, Action<string
 export const fetchRequest: AppThunk = (type:string, query: string, page: number, count: number ) => {
   return (dispatch: Dispatch) => {
     dispatch(fetchSearchsRequest());
-    axios.get(`https://api.github.com/search/${type}?q=${query}&page=${page}&per_page=${count}`)
+    axios.get(`${process.env.SEARCH_API_URL}/${type}?q=${query}&page=${page}&per_page=${count}`)
       .then((response) => {
         const searchResult: any = response;
         // response.data is the Searchs
